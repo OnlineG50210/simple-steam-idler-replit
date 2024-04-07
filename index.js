74fa4814cd251a0d2f21d8bda46d35a227319b11
OnlineG50210 <88597012+OnlineG50210@users.noreply.github.com>
2024-04-07 18:09:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [714010, 1172470, 1938090, 953490, 730, 223710, 262060, 388410, 588650, 1085660, 960170, 322330, 570, 2073850, 1284210, 1240440, 1289310, 367520, 550, 1599340, 774171], 2357570, 444090, 238960, 108600, 578080, 1343400, 1818450, 389730, 105600, 230410, 292030;  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
